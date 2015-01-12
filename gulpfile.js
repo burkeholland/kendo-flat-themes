a3bcc2db27249e0ccab2d87a41a5093f1cae4dfb
Burke Holland <burkeholland@gmail.com>
2015-01-12 08:55:31 -0600

    Update Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,12 @@ var less = require('gulp-less');
 var rename = require('gulp-rename');
 var path = require('path');
 
-gulp.task('less', function () {
+gulp.task('app', ['default'], function () {
+  gulp.src('./dist/*.css')
+    .pipe(gulp.dest('../app/public/stylesheets/themes'));
+});
+
+gulp.task('default', function() {
   gulp.src('./less/themes/**/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
@@ -11,9 +16,5 @@ gulp.task('less', function () {
     .pipe(rename(function(path) {
       path.basename = 'kendo.mobile.flat.' + path.basename;
     }))
-    .pipe(gulp.dest('../app/public/stylesheets/themes'));
-});
-
-gulp.task('default', function() {
-  // place code for your default task here
+    .pipe(gulp.dest('./dist'));
 });
